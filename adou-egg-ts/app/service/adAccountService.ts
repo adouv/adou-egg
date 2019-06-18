61c129a09fcc13a80a5860751338de464c10a9bd
adouv <doufu_0329@163.com>
2019-06-18 21:06:46 +0800

    账户表服务

--- adou-egg-ts/app/service/adAccountService.ts
+++ adou-egg-ts/app/service/adAccountService.ts
@@ -1,3 +1,4 @@
+import { adAccountModel } from './../model/adAccountModel';
 import { Service } from 'egg';
 /**
  * 账户服务类
@@ -18,9 +19,64 @@ export default class AdAccountService extends Service {
      * @return 
      * @memberof AdAccountService
      */
-    public async getList(where?: any) {
+    public async get(where?: adAccountModel): Promise<any> {
+        const { app } = this;
+        const result: any = await app.mysql.get(`${this.table_name}`, where);
+        return result;
+    }
+    /**
+     * 
+     * @param  {adAccountModel} [where] 
+     * @return Promise<any> 
+     * @memberof AdAccountService
+     */
+    public async select(where?: adAccountModel): Promise<any> {
         const { app } = this;
         const result: any = await app.mysql.select(`${this.table_name}`, where);
+        return result;
+    }
+    /**
+     * 
+     * @param  {adAccountModel} [where] 
+     * @return Promise<any> 
+     * @memberof AdAccountService
+     */
+    public async update(where?: adAccountModel): Promise<any> {
+        const { app } = this;
+        const result: any = await app.mysql.update(`${this.table_name}`, where);
+        return result;
+    }
+    /**
+     * 
+     * @param  {adAccountModel} [where] 
+     * @return Promise<any> 
+     * @memberof AdAccountService
+     */
+    public async delete(where?: adAccountModel): Promise<any> {
+        const { app } = this;
+        const result: any = await app.mysql.delete(`${this.table_name}`, where);
+        return result;
+    }
+    /**
+     * 
+     * @param  {adAccountModel} [where] 
+     * @return Promise<any> 
+     * @memberof AdAccountService
+     */
+    public async insert(where?: adAccountModel): Promise<any> {
+        const { app } = this;
+        const result: any = await app.mysql.insert(`${this.table_name}`, where);
+        return result;
+    }
+    /**
+     * 
+     * @param  {adAccountModel} [where] 
+     * @return Promise<any> 
+     * @memberof AdAccountService
+     */
+    public async query(where?: adAccountModel): Promise<any> {
+        const { app } = this;
+        const result: any = await app.mysql.query(`${this.table_name}`, where);
         return result;
     }
 }
