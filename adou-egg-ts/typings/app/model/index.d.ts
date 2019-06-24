13b67b8316e69e82da4875a68c19416421950453
adouv <doufu_0329@163.com>
2019-06-24 13:43:26 +0800

    重新编译ts

--- adou-egg-ts/typings/app/model/index.d.ts
+++ adou-egg-ts/typings/app/model/index.d.ts
@@ -2,24 +2,28 @@
 // Do not modify this file!!!!!!!!!
 
 import 'egg';
-import ExportAdAccountModel from '../../../app/model/adAccountModel';
 import ExportRequestBaseModel from '../../../app/model/requestBaseModel';
 import ExportRequestModel from '../../../app/model/requestModel';
 import ExportResponseMessageModel from '../../../app/model/responseMessageModel';
 import ExportRouterModel from '../../../app/model/routerModel';
-import ExportRequestRequestAccountModel from '../../../app/model/request/requestAccountModel';
-import ExportRequestRequestUserModel from '../../../app/model/request/requestUserModel';
+import ExportAccountRequestRequestAccountModel from '../../../app/model/account/request/requestAccountModel';
+import ExportUserRequestRequestUserModel from '../../../app/model/user/request/requestUserModel';
 
 declare module 'egg' {
   interface IModel {
-    AdAccountModel: ReturnType<typeof ExportAdAccountModel>;
     RequestBaseModel: ReturnType<typeof ExportRequestBaseModel>;
     RequestModel: ReturnType<typeof ExportRequestModel>;
     ResponseMessageModel: ReturnType<typeof ExportResponseMessageModel>;
     RouterModel: ReturnType<typeof ExportRouterModel>;
-    Request: {
-      RequestAccountModel: ReturnType<typeof ExportRequestRequestAccountModel>;
-      RequestUserModel: ReturnType<typeof ExportRequestRequestUserModel>;
+    Account: {
+      Request: {
+        RequestAccountModel: ReturnType<typeof ExportAccountRequestRequestAccountModel>;
+      }
+    }
+    User: {
+      Request: {
+        RequestUserModel: ReturnType<typeof ExportUserRequestRequestUserModel>;
+      }
     }
   }
 }
--- adou-egg-ts/typings/app/service/index.d.ts
+++ adou-egg-ts/typings/app/service/index.d.ts
@@ -5,11 +5,15 @@ import 'egg';
 import ExportAdAccountService from '../../../app/service/adAccountService';
 import ExportAdUserService from '../../../app/service/adUserService';
 import ExportBaseService from '../../../app/service/baseService';
+import ExportAccountAdAccountService from '../../../app/service/account/adAccountService';
 
 declare module 'egg' {
   interface IService {
     adAccountService: ExportAdAccountService;
     adUserService: ExportAdUserService;
     baseService: ExportBaseService;
+    account: {
+      adAccountService: ExportAccountAdAccountService;
+    }
   }
 }
